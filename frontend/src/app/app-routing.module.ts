0447bc0798e60c52e4492e5d6fee788d66895d8e
aymanzrari <aymanzrari@gmail.com>
2021-02-15 15:06:45 +0100

    add router frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',  
+    component:LoginComponent
+  },
+  {
+    path:'signup',  
+    component:SignupComponent
+  },
+  {
+    path:'profile',  
+    component:ProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
